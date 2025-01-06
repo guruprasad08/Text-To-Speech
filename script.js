4fda913a4dabb87f649ca291dd3b53e0d648f24c
gp <guruprasad26gp@gmail.com>
2025-01-06 18:07:23 +0530

    main

---
+++ script.js
@@ -0,0 +1,10 @@
+function textToAudio() {
+    let msg = document.querySelector('.text').value;
+    let speech = new SpeechSynthesisUtterance();
+    speech.lang = 'en-ko';
+    speech.text = msg;
+    speech.volume = 1;
+    speech.rate = 1;
+    speech.pitch = 1;
+    speechSynthesis.speak(speech);   
+}
